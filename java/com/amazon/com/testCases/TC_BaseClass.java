a3d8a1f01b9d6852170908782e0918e1a9dd3469
Kumudam Vasudevan <34899342+KumudamVasudevan@users.noreply.github.com>
2022-01-12 12:17:27 -0800

    Update TC_BaseClass.java

--- java/com/amazon/com/testCases/TC_BaseClass.java
+++ java/com/amazon/com/testCases/TC_BaseClass.java
@@ -10,8 +10,8 @@ import org.apache.log4j.PropertyConfigurator;
 public class TC_BaseClass {
 
 	public String baseURL = "https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&";
-	public String usrName = "9894527551";
-	public String password = "Happy24";
+	public String usrName = "yourid";
+	public String password = "youraccountpwd";
 	public static WebDriver driver;
 	public static Logger logger;
 
